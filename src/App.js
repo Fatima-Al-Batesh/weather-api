18602457af76441de5e37328e531970adafde418
Fatima Al-Batesh <fbatesh@gmail.com>
2020-10-14 14:30:45 +0300

    Weather API: Breaking the HTML in App.js into React components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import Search from "./components/Search";
-
-
-//import SayHi, { SayHello } from "./components/WeatherItem";
+import HoursWeather from "./components/24HoursWeather";
+import WeatherNow from "./components/WeatherItem";
 //import fakeWeatherData from "./fakeWeatherData.json";
 
 import "./App.css";
@@ -23,64 +22,8 @@ class App extends Component {
     return (
       <div className="app">
         <Search className="header" handleInput={this.handleInputChange} />
-           <main className="content">
-            <div> 
-              <img src="http://placekitten.com/100/100" alt="cat"/>
-            </div>
-            <div>
-             <span><strong>Temperature</strong></span> 10 &#176;C to 11 &#176;C
-            </div>
-           
-            <ul>
-              <li><strong>Humidity</strong></li>
-              <li> 76%</li>
-              <li><strong>Pressure</strong></li>
-              <li> 1008.48</li>
-            </ul>
-           
-           </main>
-         
-
-        <section className="hour-weather">
-            <ul className="hour-weather-t">
-              <li className="threeAM">
-                <time>03:30</time>
-                <img src="http://placekitten.com/40/40" alt="cat"/>
-                <p>8&#176;C</p>
-              </li>
-              <li className="sixAM">
-                <time>06:00</time>
-                <img src="http://placekitten.com/40/40"   alt="cat"/>
-                <p>9&#176;C</p>
-              </li>
-              <li className="nineAM">
-                <time>09:00</time>
-                <img src="http://placekitten.com/40/40"   alt="cat"/>
-                <p>14&#176;C</p>
-              </li>
-              <li className="twelvePM">
-                <time>09:00</time>
-                <img src="http://placekitten.com/40/40"  alt="cat"/>
-                <p>17&#176;C</p>
-              </li>
-              <li className="threePM">
-                <time>15:00</time>
-                <img src="http://placekitten.com/40/40"   alt="cat"/>
-                <p>18&#176;C</p>
-              </li>
-              <li className="sixPM">
-                <time>18:00</time>
-                <img src="http://placekitten.com/40/40"   alt="cat"/>
-                <p>16&#176;C</p>
-              </li>
-              <li className="ninePM">
-                <time>21:00</time>
-                <img src="http://placekitten.com/40/40"  alt="cat"/>
-                <p>13&#176;C</p>
-              </li>
-                
-            </ul>
-        </section>
+            <WeatherNow/>
+            <HoursWeather/>
       </div>
     );
   }
--- src/components/WeatherItem.js
+++ src/components/WeatherItem.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
+import mostlycloudy from "./../img/weather-icons/mostlycloudy.svg";
 import { render } from "react-dom";
+import "./../App.css";
 
-export default class WeatherItem extends Component {
-  render() {
-    return <div>Heello</div>;
-  }
-}
-export class SayHello extends Component {
+export default class WeatherNow extends Component {
   render() {
     return (
-      <div style={{ color: this.props.color, backgroundColor: "yellow" }}>
-        Hello {this.props.name}
-      </div>
+      <main className="content">
+            <div> 
+            <img src={mostlycloudy} alt="mostlycloudy icon" />
+            </div>
+            <div>
+             <span><strong>Temperature</strong></span> 10 &#176;C to 11 &#176;C
+            </div>
+           
+            <ul>
+              <li><strong>Humidity</strong></li>
+              <li> 76%</li>
+              <li><strong>Pressure</strong></li>
+              <li> 1008.48</li>
+            </ul>
+           
+           </main>
     );
   }
 }
